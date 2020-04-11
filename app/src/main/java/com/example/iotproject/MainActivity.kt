b7f4c52396af920971bd6c41fa7d0b714e0a0a86
Nchen118 <nchen118@yahoo.com>
2020-04-11 16:07:37 +0800

    kick start

--- app/src/main/java/com/example/iotproject/MainActivity.kt
+++ app/src/main/java/com/example/iotproject/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.iotproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.*
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        txt_welcome.visibility = View.GONE
+        txt_to.visibility = View.GONE
+        txt_smartroom.visibility = View.GONE
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        TransitionManager.beginDelayedTransition(Constraint_Layout, Fade().setDuration(1000).setStartDelay(5000))
+
+        txt_welcome.visibility = View.VISIBLE
+        txt_to.visibility = View.VISIBLE
+        txt_smartroom.visibility = View.VISIBLE
     }
 }
