a3d0fd51ab1ed263b30a8fd3a93ffef0023ae140
Nchen118 <nchen118@yahoo.com>
2020-04-20 03:37:57 +0800

    panel UI improve and bug fixed

--- app/src/main/java/com/example/iotproject/fan_setting.kt
+++ app/src/main/java/com/example/iotproject/fan_setting.kt
@@ -2,11 +2,11 @@ package com.example.iotproject
 
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -14,147 +14,90 @@ import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_fan_setting.*
 
 class fan_setting : AppCompatActivity() {
-
-
     private var fanValue = 0
     private var auto = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val roomId = intent.getStringExtra("RoomId");
-        val database = FirebaseDatabase.getInstance()
-        val fan = database.getReference("/Room/$roomId/fan")
-        val fanAuto = database.getReference("/Room/$roomId/fanAuto")
-        val temp = database.getReference("/Room/$roomId/temp")
         setContentView(R.layout.activity_fan_setting)
 
         val actionbar = supportActionBar
         actionbar!!.title = "Adjust Fan Speed"
-        //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
-        FanTitle.text = "The current fan speed"
-        autoBtn.isEnabled = true
-        fanAuto.addValueEventListener(object: ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
 
-            }
+        val roomId = intent.getStringExtra("RoomId")
+        val database = FirebaseDatabase.getInstance()
+        val fan = database.getReference("/Room/$roomId/fan")
+        val fanAuto = database.getReference("/Room/$roomId/fanAuto")
+
+        fanAuto.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {}
+
             override fun onDataChange(p0: DataSnapshot) {
-                val post = p0.getValue(String::class.java) ?: return
-                if(post=="1"){
-                    autoOn()
-                }else if(post=="0"){
-                    autoOff()
-                }else{
-                    autoBtn.isEnabled = false
-                }
+                val post = p0.value.toString()
+                if (post == "1") autoSwitch.isChecked = true
+                else if (post == "0") autoSwitch.isChecked = false
             }
         })
-        fan.addValueEventListener(object: ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
 
-            }
+        fan.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {}
+
             override fun onDataChange(p0: DataSnapshot) {
-                val post = p0.getValue(String::class.java) ?: ""
-                if(post==""){
-                    increase.isEnabled = false
-                    decrease.isEnabled = false
-                    FanSpeedText.text = "Error"
-                }else{
-                    FanSpeedText.text = post
+                val post = p0.value.toString()
+                if (p0.value.toString().isNotEmpty()) {
                     fanValue = post.toInt()
-                    if(fanValue== 0){
-                        increase.setBackgroundColor(Color.GREEN)
-                        increase.isEnabled = true
-                        decrease.setBackgroundColor(Color.GRAY)
-                        decrease.isEnabled = false
-                    }else if(fanValue == 5){
-                        increase.setBackgroundColor(Color.GRAY)
-                        increase.isEnabled = false
-                        decrease.setBackgroundColor(Color.GREEN)
-                        decrease.isEnabled = true
-                    }else{
-                        increase.setBackgroundColor(Color.GREEN)
-                        increase.isEnabled = true
-                        decrease.setBackgroundColor(Color.GREEN)
-                        decrease.isEnabled = true
+                    when {
+                        fanValue <= 0 -> {
+                            decrease.setBackgroundColor(Color.GRAY)
+                            decrease.isEnabled = false
+                        }
+                        fanValue >= 5 -> {
+                            increase.setBackgroundColor(Color.GRAY)
+                            increase.isEnabled = false
+                        }
+                        else -> {
+                            increase.setBackgroundColor(Color.parseColor("#333333"))
+                            increase.isEnabled = true
+                            decrease.setBackgroundColor(Color.parseColor("#333333"))
+                            decrease.isEnabled = true
+                        }
                     }
                     updateText()
                 }
 
             }
         })
+
         decrease.setOnClickListener {
-            autoOff()
+            autoSwitch.isChecked = false
             fanAuto.setValue("0")
-            if(fanValue==0){
-                updateText()
-            }
-            else{
+            if (fanValue > 0) {
                 fanValue--
                 fan.setValue(fanValue.toString())
                 updateText()
-                increase.setBackgroundColor(Color.GREEN)
-                increase.isEnabled = true
             }
         }
-        increase.setOnClickListener{
-            autoOff()
+
+        increase.setOnClickListener {
+            autoSwitch.isChecked = false
             fanAuto.setValue("0")
-            if(fanValue==5){
-                updateText()
-            }
-            else{
+            if (fanValue < 5) {
                 fanValue++
                 fan.setValue(fanValue.toString())
                 updateText()
-                decrease.setBackgroundColor(Color.GREEN)
-                decrease.isEnabled = true
             }
         }
-        autoBtn.setOnClickListener {
-            if(!auto){
-                auto = true
-                fanAuto.setValue("1")
-                autoOn()
-            }else{
-                auto = false
-                fanAuto.setValue("0")
-                autoOff()
-            }
+
+        autoSwitch.setOnCheckedChangeListener { compoundButton: CompoundButton, b: Boolean ->
+            auto = b
+            if (b) fanAuto.setValue("1")
+            else fanAuto.setValue("0")
         }
     }
-    fun updateText(){
+
+    fun updateText() {
         FanSpeed.text = fanValue.toString()
-    }
-    fun autoOn(){
-        auto = true
-        autoBtn.setBackgroundColor(Color.GREEN)
-        autoText.visibility = View.VISIBLE
-    }
-    fun autoOff(){
-        auto = false
-        autoBtn.setBackgroundColor(Color.RED)
-        autoText.visibility = View.INVISIBLE
-        when (fanValue) {
-            0 -> {
-                increase.setBackgroundColor(Color.GREEN)
-                increase.isEnabled = true
-                decrease.setBackgroundColor(Color.GRAY)
-                decrease.isEnabled = false
-            }
-            5 -> {
-                increase.setBackgroundColor(Color.GRAY)
-                increase.isEnabled = false
-                decrease.setBackgroundColor(Color.GREEN)
-                decrease.isEnabled = true
-            }
-            else -> {
-                increase.setBackgroundColor(Color.GREEN)
-                increase.isEnabled = true
-                decrease.setBackgroundColor(Color.GREEN)
-                decrease.isEnabled = true
-            }
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -162,18 +105,19 @@ class fan_setting : AppCompatActivity() {
         this.finish()
         return true
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar menu items
-        if(item.itemId == R.id.qrCode){
+        if (item.itemId == R.id.qrCode) {
             startActivity(Intent(this, QRCodeGenerator::class.java))
             return true
         }
-
         return super.onOptionsItemSelected(item)
     }
 }
